3995b48c01e78b109e32284df745a2c5e06126fd
unknown <imxotepalex@gmail.com>
2019-11-24 22:03:19 +0300

    Add task2

---
+++ Task 2/Example.java
@@ -0,0 +1,13 @@
+package Methods;
+
+public class Example {
+    public static void main(String[] args) {
+
+        Methods methods = new Methods();
+        System.out.println("Min value is - " + methods.minNumber(2, 3));
+        System.out.println("Value is even - " + methods.evenValue(2));
+        System.out.println("Sqrt of the value is - " + methods.sqrtValue(2.0));
+        System.out.println("Cube of the value is - " + methods.cubeValue(2.1));
+    }
+}
+
---
+++ Task 2/Methods.java
@@ -0,0 +1,19 @@
+package Methods;
+
+public class Methods {
+    public static void main(String[] args) {
+
+    }
+    public static int minNumber(int a, int b) {
+        return (Math.min(a, b));
+    }
+    public static boolean evenValue(int a) {
+        return (a % 2 == 0);
+    }
+    public static double sqrtValue(double a) {
+        return (Math.pow(a, 2));
+    }
+    public static double cubeValue(double a) {
+        return (Math.pow(a, 3));
+    }
+}
