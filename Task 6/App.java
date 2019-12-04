6017c6f3f56d840a9d127d78722247249d20cdf3
Aleksey <imxotepalex@gmail.com>
2019-12-04 22:40:41 +0300

    Add Task 6

---
+++ Task 6/App.java
@@ -0,0 +1,17 @@
+package pvt;
+
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {}
+    public String summaAB(String a, String b) {
+        return (a + b);
+    }
+
+    public String otherWiseAB(String a, String b) {
+        return (b+a);
+    }
+}
+
---
+++ Task 6/AppTest7.java
@@ -0,0 +1,24 @@
+package pvt;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AppTest7 {
+   private String A = "Stay ";
+   private String B = "Calm";
+
+    @Test
+    public void testsummaAB() {
+        App app = new App();
+        String actual = app.summaAB(A,B);
+        String expected = "Stay Calm";
+        Assert.assertEquals(actual,expected);
+    }
+
+    @Test
+    public void testotherWiseAB() {
+        App app = new App();
+        String actual = app.summaAB(B,A);
+        String expected = "CalmStay ";
+        Assert.assertEquals(actual,expected);
+    }
+}
